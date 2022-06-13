b4059577f98b7240c900f9c534cf75e4439fc186
tkgstrator <nasawake.am@gmail.com>
2022-06-14 01:38:32 +0900

    - DeepLinkのバグ修正

--- lib/main.dart
+++ lib/main.dart
@@ -77,6 +77,16 @@ class _HomeViewState extends State<HomeView> {
   }
 }
 
+class SalmonStatsView extends StatefulWidget {
+  @override
+  _SalmonStatsState createState() => _SalmonStatsState();
+}
+
+class _SalmonStatsState extends State<SalmonStatsView> {
+  @override
+  Widget build(BuildContext context) {}
+}
+
 class SettingView extends StatefulWidget {
   @override
   _SettingViewState createState() => _SettingViewState();
@@ -84,6 +94,8 @@ class SettingView extends StatefulWidget {
 
 class _SettingViewState extends State<SettingView> {
   bool _isForceUpdated = false;
+  Uri oauthURL = Uri.parse(
+      "https://accounts.nintendo.com/connect/1.0.0/authorize?state=V6DSwHXbqC4rspCn_ArvfkpG1WFSvtNYrhugtfqOHsF6SYyX&redirect_uri=npf71b963c1b7b6d119://auth&client_id=71b963c1b7b6d119&scope=openid+user+user.birthday+user.mii+user.screenName&response_type=session_token_code&session_token_code_challenge=tYLPO5PxpK-DTcAHJXugD7ztvAZQlo0DQQp3au5ztuM&session_token_code_challenge_method=S256&theme=login_form");
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -107,8 +119,7 @@ class _SettingViewState extends State<SettingView> {
                 trailing: OutlinedButton(
                   child: const Text("Login"),
                   onPressed: () {
-                    launchUrl(
-                        Uri.parse("https://api-dev.splatnet2.com/documents"));
+                    launchUrl(oauthURL);
                   },
                 )),
             const ListTile(
